0cd71cdf6a2ba260a6337b6fcbfdfe8a58090398
johan <johan@exclude.se>
2015-12-05 20:23:02 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import TCPServer.TCPServer;
+
+import java.io.IOException;
+
+/**
+ * Created by johan on 2015-12-05.
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TCPServer tcpServer = new TCPServer(1234);
+            tcpServer.startServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/TCPServer/TCPServer.java
@@ -0,0 +1,39 @@
+package TCPServer;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by johan on 2015-12-05.
+ */
+public class TCPServer {
+    private int port;
+    private boolean runMe;
+    ServerSocket serverSocket;
+    public TCPServer(int port) throws IOException {
+        this.port = port;
+        this.runMe = true;
+        serverSocket = new ServerSocket(port);
+    }
+    public void startServer() throws IOException {
+        String clientData;
+        System.out.println("Starting new TCP-server on port" + this.port);
+        while (runMe){
+            Socket socket = serverSocket.accept();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            System.out.println("GOT A CONNECTION FROM: " + socket.getRemoteSocketAddress().toString());
+            while(socket.isConnected()){
+                clientData = bufferedReader.readLine();
+                System.out.println("GOT: " + clientData);
+                outputStream.writeBytes("OK\n\r");
+            }
+
+        }
+    }
+
+}
